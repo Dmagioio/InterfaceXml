961c9d9eb3117c3c6fcfb2febf6e0fb495749428
marki <markian3838@gmail.com>
2025-10-15 22:28:33 +0300

    learn implement toast, snackbar, and animations

--- app/src/main/java/com/example/learntestcomplete/MainActivity.kt
+++ app/src/main/java/com/example/learntestcomplete/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.learntestcomplete
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.animation.AnimationUtils
-import androidx.activity.result.launch
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.learntestcomplete.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 
@@ -21,13 +24,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnClick.setOnClickListener {
-            binding.tvReplacementWord.text = "Dood job!"
-            binding.tvReplacementAndroid.text = "Dood job!"
+            binding.tvReplacementWord.text = "Good job!"
+            binding.tvReplacementAndroid.text = "Good job!"
 
             lifecycleScope.launch {
                 var timerValue = binding.tvTimer.text.toString().toIntOrNull() ?: 0
 
-                while (timerValue < 1000) {
+                while (isActive) {
                     delay(1000L)
                     timerValue++
                     binding.tvTimer.text = timerValue.toString()
@@ -38,8 +41,24 @@ class MainActivity : AppCompatActivity() {
             fadeInAnimator.duration = 1000
             fadeInAnimator.start()
 
-            val slideUpFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up_and_fade_in)
-            binding.tvReplacementAndroid.startAnimation(slideUpFadeInAnimation)
+//            val slideUpFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up_and_fade_in)
+//            binding.tvReplacementAndroid.startAnimation(slideUpFadeInAnimation)
+//
+//            val inflater = layoutInflater
+//            val layout = inflater.inflate(R.layout.custom_toast, null)
+//
+//            val toast = Toast(applicationContext)
+//            toast.duration = Toast.LENGTH_SHORT
+//            toast.view = layout
+//            toast.show()
+
+            Snackbar.make(binding.root, "Файл видалено", Snackbar.LENGTH_LONG)
+                .setAction("Відмінити") {
+                    Toast.makeText(this, "Відмінено", Toast.LENGTH_SHORT).show()
+                }
+                .show()
         }
+        
+        Toast.makeText(this, "Вітаємо в нашому додатку!", Toast.LENGTH_LONG).show()
     }
 }
