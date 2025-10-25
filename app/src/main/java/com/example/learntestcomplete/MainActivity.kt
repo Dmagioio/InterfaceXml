ddf8696163b2c2a169a386f6cec10b5556873cff
marki <markian3838@gmail.com>
2025-10-25 23:41:52 +0300

    learn edit text

--- app/src/main/java/com/example/learntestcomplete/MainActivity.kt
+++ app/src/main/java/com/example/learntestcomplete/MainActivity.kt
@@ -3,15 +3,10 @@ package com.example.learntestcomplete
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
-import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
 import com.example.learntestcomplete.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonShow.setOnClickListener {
+            val name = binding.editTextName.text.toString()
+            binding.textViewResult.text = "Привіт, $name!"
+        }
 
         bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
@@ -42,7 +42,6 @@ class MainActivity : AppCompatActivity() {
                     BottomSheetBehavior.STATE_HIDDEN -> binding.tvTitle.text = "Сховано ❌"
                 }
             }
-
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
             }
         })
@@ -58,62 +57,5 @@ class MainActivity : AppCompatActivity() {
             }
         }.attach()
 
-        Glide.with(this)
-            .load("https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/10-intelligent-dog-breeds/golden-retriever-tongue-out.jpg?h=430&w=710&hash=7FEB820D235A44B76B271060E03572C7")
-            .into(binding.imageView)
-
-        binding.imageView.alpha = 0f
-        ObjectAnimator.ofFloat(binding.imageView, "alpha", 0f, 1f).apply {
-            duration = 1000
-            start()
-        }
-
-        binding.btnClick.setOnClickListener {
-            binding.tvReplacementWord.text = "Good job!"
-            binding.tvReplacementAndroid.text = "Good job!"
-
-            lifecycleScope.launch {
-                var timerValue = binding.tvTimer.text.toString().toIntOrNull() ?: 0
-                while (timerValue < 1000) {
-                    delay(1000L)
-                    timerValue++
-                    binding.tvTimer.text = timerValue.toString()
-                }
-            }
-
-            ObjectAnimator.ofFloat(binding.tvReplacementWord, "alpha", 0f, 1f).apply {
-                duration = 1000
-                start()
-            }
-
-            val slideUpFadeInAnimation =
-                AnimationUtils.loadAnimation(this, R.anim.slide_up_and_fade_in)
-            binding.tvReplacementAndroid.startAnimation(slideUpFadeInAnimation)
-
-            binding.imageView.setImageResource(R.drawable.cat)
-            ObjectAnimator.ofFloat(binding.imageView, "alpha", 0f, 1f).apply {
-                duration = 1000
-                start()
-            }
-        }
-
-        binding.btnClick2.setOnClickListener {
-            binding.imageView.setImageResource(R.drawable.dog)
-            ObjectAnimator.ofFloat(binding.imageView, "alpha", 0f, 1f).apply {
-                duration = 1000
-                start()
-            }
-        }
-
-        binding.imageView.setOnClickListener {
-            Glide.with(this)
-                .asGif()
-                .load(R.drawable.cat2)
-                .into(binding.imageView)
-        }
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, ExampleFragment())
-            .commit()
     }
 }
